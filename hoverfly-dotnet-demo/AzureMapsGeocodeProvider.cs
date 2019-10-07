bc6a5f32411b2fe1866c9b14cffaf4fdc1d13928
Eduardo Monteiro de Barros <eduardo.mb@amwins.com>
2019-10-07 12:14:29 -0400

    removed unnecessary usings

--- hoverfly-dotnet-demo/AzureMapsGeocodeProvider.cs
+++ hoverfly-dotnet-demo/AzureMapsGeocodeProvider.cs
@@ -1,10 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace hoverfly_dotnet_demo
